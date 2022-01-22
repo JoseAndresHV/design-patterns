32effcf4822279ae342acaf60194bbfb8df287e0
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-01-22 15:08:51 -0400

    Add SingletonAspDotNet: Singleton Pattern

---
+++ Singleton/SingletonAspDotNet/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SingletonAspDotNet.Configuration;
+using SingletonAspDotNet.Models;
+using SingletonAspDotNet.Tools;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingletonAspDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptions<MyConfig> _config;
+        public HomeController(IOptions<MyConfig> config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            Log.GetInstance(_config.Value.PathLog).Save("You got into Index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Log.GetInstance(_config.Value.PathLog).Save("You got into Privacy");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Singleton/SingletonAspDotNet/Tools/Log.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingletonAspDotNet.Tools
+{
+    public sealed class Log
+    {
+        private static Log _instance = null;
+        private string _path;
+
+        public static Log GetInstance(string path)
+        {
+            if (_instance == null)
+            {
+                _instance = new Log(path);
+            }
+
+            return _instance;
+        }
+
+        private Log(string path)
+        {
+            _path = path;
+        }
+
+        public void Save(string message)
+        {
+            File.AppendAllText(_path, DateTime.Now + " -> " + message + Environment.NewLine);
+        }
+    }
+}
